0dcb5d2edd6f4a0f88940a0abd00c4f104c020e7
MariamZead <mariam.a.zead@gmail.com>
2023-12-06 21:57:47 +0200

    Understanding how to handle function declarations

---
+++ Fundamentals/JS/Functions.js
@@ -0,0 +1,41 @@
+// ?==================== Function Declarations ====================?
+
+/**
+ * ! can be invoked before they are defined (Hoisting)
+ * * default value is Undefined for (parammeters, return)
+ */
+
+function calcSum1(x, y, z) {
+  var sum = x + y + z;
+  console.log(sum);
+}
+// ? overrides the above function
+function calcSum1(x, y) {
+  var sum = x + y;
+  return sum;
+  // Unreachable code
+  console.log(sum);
+}
+
+// ignoring the third value
+console.log(calcSum1(2, 3, 5));
+// NaN y default value is Undefined
+console.log(calcSum1(2));
+
+
+function calcSum2(x, y) {
+  var sum = x + y;
+}
+
+// default value of the return is Undefined
+console.log(calcSum2(2, 3)); 
+
+
+
+function calcAge1(birthyear) {
+  const age = 2023 - birthyear;
+  return age;
+}
+
+const age1 = calcAge1(2002);
+console.log(age1);

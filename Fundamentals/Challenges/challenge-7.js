594a803c53b6222748e909dc9c5a248e64dce437
MariamZead <mariam.a.zead@gmail.com>
2023-12-07 10:21:41 +0200

    Modifying the code.

--- Fundamentals/Challenges/challenge-7.js
+++ Fundamentals/Challenges/challenge-7.js
@@ -9,31 +9,37 @@
  */
 
 
-// ? function expression takes bill value and return the tip
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
+
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totals = [];
+
+
+// ? function expression takes bill value and return the tip
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 for (let i = 0; i < bills.length; i++) {
   const tip = calcTip(bills[i]);
   tips.push(tip);
   totals.push(tip + bills[i]);
 }
-console.log(bills, tips, totals);
 
+console.log(`bills array : ${bills}`);
+console.log(`tips array : ${tips}`);
+console.log(`totals array : ${totals}`);
 
 // ? function expression takes array and returns the avg
 const calcAverage = function (arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
-    // sum = sum + arr[i];
     sum += arr[i];
   }
-  return sum / arr.length;
+  const avg = sum / arr.length;
+  return avg;
 };
-console.log(calcAverage([2, 3, 7]));
-console.log(calcAverage(totals));
-console.log(calcAverage(tips));
+
+const arr = [8, 3, 5];
+console.log(calcAverage(arr).toFixed(2));
+
+console.log(`The totals average : ${calcAverage(totals)}`);

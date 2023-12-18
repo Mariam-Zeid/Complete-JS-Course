0836ae572409643d920774fe759879227cdd7977
MariamZead <mariam.a.zead@gmail.com>
2023-12-18 12:15:39 +0200

    Creating a function to clear the input fields

--- DOM Projects/Simple CRUD System/js/script.js
+++ DOM Projects/Simple CRUD System/js/script.js
@@ -4,15 +4,17 @@ const productCategoryInput = document.getElementById("productCategory");
 const productSaleInput = document.getElementById("productSale");
 const productDescriptionInput = document.getElementById("productDescription");
 
+const addProductBtn = document.getElementById("addProduct");
+const clearFormBtn = document.getElementById("clearForm");
+
 // ? Products Array
-const productsArr = [];
+let productsArr = [];
 
 // ? Display Data
 const displayData = function () {
   let temp = "";
   for (let i = 0; i < productsArr.length; i++) {
-    temp += `
-     <tr>
+    temp += `<tr>
           <td>${i}</td>
           <td>${productsArr[i].productName}</td>
           <td>${productsArr[i].productPrice}</td>
@@ -25,27 +27,34 @@ const displayData = function () {
           <td>
             <button class="btn btn-danger">delete</button>
           </td>
-        </tr>
-     `;
+        </tr>`;
   }
   document.getElementById("tableData").innerHTML = temp;
 };
 
-const addProductBtn = document
-  .getElementById("addProduct")
-  .addEventListener("click", function () {
-    // ? Product Object
-    const productObj = {
-      productName: productNameInput.value,
-      productPrice: productPriceInput.value,
-      productCategory: productCategoryInput.value,
-      productSale: productSaleInput.value,
-      productDescription: productDescriptionInput.value,
-    };
+// ? Adding product to the array
+addProductBtn.addEventListener("click", function () {
+  // ? Product Object
+  const productObj = {
+    productName: productNameInput.value,
+    productPrice: productPriceInput.value,
+    productCategory: productCategoryInput.value,
+    productSale: productSaleInput.value,
+    productDescription: productDescriptionInput.value,
+  };
 
-    // ? Push Product Object to Products Array (kol mara hazawed obj )
-    productsArr.push(productObj);
+  // ? Push Product Object to Products Array (kol mara hazawed obj )
+  productsArr.push(productObj);
 
-    // ? Display Object Properties in the table
-    displayData();
-  });
+  // ? Display Object Properties in the table
+  displayData();
+});
+
+// ? clearing the inputs
+clearFormBtn.addEventListener("click", function () {
+  productNameInput.value = "";
+  productPriceInput.value = "";
+  productCategoryInput.value = "Select Category";
+  productSaleInput.checked = false;
+  productDescriptionInput.value = "";
+});
